fa80b7c88f7457463eee397c5c8d38acad94a575
耗子 <i@haozi.net>
2024-06-22 02:40:04 +0800

    feat: 优化插件目录

--- wp-content/themes/wenpai-plugins/functions.php
+++ wp-content/themes/wenpai-plugins/functions.php
@@ -3,12 +3,20 @@
 defined( 'ABSPATH' ) || exit;
 
 
+/**
+ * 全局静态文件引入
+ */
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'wenpai-plugins', get_stylesheet_directory_uri() . '/assets/css/directory.css' );
+} );
+
+
 // 目录 meta 区块过滤
 add_filter( 'meta_field_block_get_block_content', function ( $block_content, $attributes, $block, $post_id, $object_type ) {
 	$field_name = $attributes['fieldName'] ?? '';
+	global $post;
 
 	if ( 'rating' === $field_name ) {
-		global $post;
 		$rating = get_post_meta( $post->ID, 'rating', true );
 		$rating = (int) $rating / 20;
 		// 渲染成星星
@@ -17,21 +25,16 @@ add_filter( 'meta_field_block_get_block_content', function ( $block_content, $at
 			$block_content .= '<span class="dashicons dashicons-star-' . ( $i < $rating ? 'filled' : 'empty' ) . '"></span>';
 		}
 		$block_content .= '</div>';
-
-		return $block_content;
 	}
 	if ( 'updated_at' === $field_name ) {
-		global $post;
 		$time          = get_the_modified_date( 'Y-m-d H:i:s', $post );
 		$block_content = human_time_diff( strtotime( $time ), current_time( 'timestamp' ) ) . '前';
 	}
 	if ( 'created_at' === $field_name ) {
-		global $post;
 		$time          = get_the_date( 'Y-m-d H:i:s', $post );
 		$block_content = human_time_diff( strtotime( $time ), current_time( 'timestamp' ) ) . '前';
 	}
 	if ( 'screenshots' === $field_name ) {
-		global $post;
 		$image_ids     = get_post_meta( $post->ID, 'screenshots', true );
 		$block_content = '';
 		if ( ! empty( $image_ids ) ) {
@@ -41,6 +44,18 @@ add_filter( 'meta_field_block_get_block_content', function ( $block_content, $at
 			}
 			$block_content .= '</div>';
 		}
+	}
+	if ( 'views' === $field_name ) {
+		$views         = (int) get_post_meta( $post->ID, 'views', true );
+		if ( $views < 10000 ) {
+			$block_content = $views;
+		} elseif ( $views < 1000000 ) {
+			$summary       = round( $views / 10000, 1 );
+			$block_content = $summary . ' 万';
+		} else {
+			$block_content = '100 万+';
+		}
+		$block_content .= '个';
 	}
 
 	return $block_content;
--- wp-content/themes/wenpai-themes/functions.php
+++ wp-content/themes/wenpai-themes/functions.php
@@ -6,19 +6,17 @@ defined( 'ABSPATH' ) || exit;
 // 目录 meta 区块过滤
 add_filter( 'meta_field_block_get_block_content', function ( $block_content, $attributes, $block, $post_id, $object_type ) {
 	$field_name = $attributes['fieldName'] ?? '';
+	global $post;
 
 	if ( 'updated_at' === $field_name ) {
-		global $post;
 		$time          = get_the_modified_date( 'Y-m-d H:i:s', $post );
 		$block_content = human_time_diff( strtotime( $time ), current_time( 'timestamp' ) ) . '前';
 	}
 	if ( 'created_at' === $field_name ) {
-		global $post;
 		$time          = get_the_date( 'Y-m-d H:i:s', $post );
 		$block_content = human_time_diff( strtotime( $time ), current_time( 'timestamp' ) ) . '前';
 	}
 	if ( 'screenshots' === $field_name ) {
-		global $post;
 		$image_ids     = get_post_meta( $post->ID, 'screenshots', true );
 		$block_content = '';
 		if ( ! empty( $image_ids ) ) {
